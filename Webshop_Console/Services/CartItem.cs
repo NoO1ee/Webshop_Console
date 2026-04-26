1160060b77a7ab29f5cddfbfba165b77571948cb
Dominik <130901818+NoO1ee@users.noreply.github.com>
2026-04-26 22:37:40 +0200

    Update CartItem.cs

--- Webshop_Console/Services/CartItem.cs
+++ Webshop_Console/Services/CartItem.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +10,6 @@ namespace Webshop_Console.Services;
 public class CartItem
 {
     public ArticleModel Article { get; set; }
+    public int Quantity { get; set; }
     public int Quantity { get; set; }
 }
